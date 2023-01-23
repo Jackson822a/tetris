be1b6b6d6dc9fadd82984f9a2ae118ed5329ec52
Jackson Adair <jackadai822@ed.amdsb.ca>
2023-01-23 10:41:30 -0500

    Add project files.

---
+++ tetris/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tetrisLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tetrisLabel
+            // 
+            this.tetrisLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tetrisLabel.Font = new System.Drawing.Font("Consolas", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tetrisLabel.ForeColor = System.Drawing.Color.White;
+            this.tetrisLabel.Location = new System.Drawing.Point(211, 88);
+            this.tetrisLabel.Name = "tetrisLabel";
+            this.tetrisLabel.Size = new System.Drawing.Size(475, 156);
+            this.tetrisLabel.TabIndex = 0;
+            this.tetrisLabel.Text = "TETRIS";
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Consolas", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(227, 274);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(432, 100);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "Press Enter to start ";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(873, 677);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.tetrisLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tetrisLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ tetris/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace tetris
+{
+    public partial class Form1 : Form
+    {
+        Rectangle Tpiece1 = new Rectangle(230, 30, 20, 20); //start left
+        Rectangle Tpiece4 = new Rectangle(250, 30, 20, 20); //start middle
+        Rectangle Tpiece3 = new Rectangle(270, 30, 20, 20); //start right
+        Rectangle Tpiece2 = new Rectangle(250, 10, 20, 20); //start top
+        Rectangle Tpiece5 = new Rectangle(250, 50, 20, 20); //invis, rotate. start bottom
+        Rectangle GameBorderL = new Rectangle(150, 30, 20, 420);
+        Rectangle GameBorderR = new Rectangle(370, 30, 20, 420);
+        Rectangle GameBorderT = new Rectangle(150, 30, 220, 20);
+        Rectangle GameBorderB = new Rectangle(150, 430, 220, 20);
+
+        
+        //List<Rectangle> Tpieces = new List<Rectangle>();
+      
+        
+      
+        List<List<Rectangle>> pieces = new List<List<Rectangle>>();
+       
+
+        int playerScore = 0;
+
+        string gameState = "waiting";
+
+        bool aDown = false; 
+        bool dDown = false;
+        bool jDown = false;
+       
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush purpleBrush = new SolidBrush(Color.MediumPurple);
+        public Form1()
+        {
+            InitializeComponent();
+            //pieces.Add(Tpiece1);
+            //pieces.Add(Tpiece2);
+            //pieces.Add(Tpiece3);
+            //pieces.Add(Tpiece4);
+            //pieces.Add(Tpiece5);
+
+           // pieces[0].Add(Tpiece1);
+            //pieces[1].Add(Tpiece4);
+            //pieces[2].Add(Tpiece2);
+            //pieces[3].Add(Tpiece3);
+            //pieces[4].Add(Tpiece1);
+            //Rectangle test = pieces[0][0];
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //move piece down
+            Tpiece1.Y += 20;
+            Tpiece4.Y += 20;
+            Tpiece3.Y += 20;
+            Tpiece2.Y += 20;
+            Tpiece5.Y += 20;
+
+            //left right
+            if (aDown == true)
+            {
+                Tpiece1.X -= 20;
+                Tpiece4.X -= 20;
+                Tpiece3.X -= 20;
+                Tpiece2.X -= 20;
+                Tpiece5.X -= 20;
+            }
+            if (dDown == true)
+            {
+                Tpiece1.X += 20;
+                Tpiece4.X += 20;
+                Tpiece3.X += 20;
+                Tpiece2.X += 20;
+                Tpiece5.X += 20;
+            }
+            //rotate
+            if (jDown == true)
+            {
+                Rectangle temp = Tpiece1;
+
+                Tpiece1 = Tpiece2;
+                Tpiece2 = Tpiece3;
+                Tpiece3 = Tpiece5;
+                Tpiece5 = temp;
+
+            }
+
+
+            //intersection
+            if (Tpiece1.IntersectsWith(GameBorderB) || Tpiece3.IntersectsWith(GameBorderB) || Tpiece2.IntersectsWith(GameBorderB))
+            {
+                Tpiece1.Y -= 20;
+                Tpiece4.Y -= 20;
+                Tpiece3.Y -= 20;
+                Tpiece2.Y -= 20;
+            }
+            if (Tpiece1.IntersectsWith(GameBorderL))
+            {
+                Tpiece1.X += 20;
+                Tpiece4.X += 20;
+                Tpiece3.X += 20;
+                Tpiece2.X += 20;
+            }
+            if (Tpiece3.IntersectsWith(GameBorderR))
+            {
+                Tpiece1.X -= 20;
+                Tpiece4.X -= 20;
+                Tpiece3.X -= 20;
+                Tpiece2.X -= 20;
+            }
+
+
+
+            Refresh();
+        }
+
+        private void GameInitalize()
+        {
+            gameState = "running";
+
+            tetrisLabel.Text = "";
+            subtitleLabel.Text = "";
+
+            timer.Enabled = true;
+
+
+
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.J:
+                    jDown = false;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.J:
+                    jDown = true;
+                    break;
+                case Keys.Enter:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameInitalize();
+                    }
+                    break;
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (gameState == "waiting")
+            {
+                tetrisLabel.Text = "TETRIS";
+                subtitleLabel.Text = "Press Enter to start";
+
+            }
+            else if (gameState == "running")
+            {
+                e.Graphics.FillRectangle(purpleBrush, Tpiece1);
+                e.Graphics.FillRectangle(purpleBrush, Tpiece4);
+                e.Graphics.FillRectangle(purpleBrush, Tpiece3);
+                e.Graphics.FillRectangle(purpleBrush, Tpiece2);
+               
+                e.Graphics.FillRectangle(whiteBrush, GameBorderL);
+                e.Graphics.FillRectangle(whiteBrush, GameBorderR);
+                e.Graphics.FillRectangle(whiteBrush, GameBorderT);
+                e.Graphics.FillRectangle(whiteBrush, GameBorderB);
+            }
+        }
+    }
+}
