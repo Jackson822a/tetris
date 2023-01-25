47582c566ce1f02bc5c6fc5147b935f0c08b2aad
Jackson Adair <jackadai822@ed.amdsb.ca>
2023-01-25 10:40:33 -0500

    change

--- tetris/Form1.cs
+++ tetris/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Diagnostics;
 
 namespace tetris
 {
@@ -23,22 +24,20 @@ namespace tetris
         Rectangle GameBorderT = new Rectangle(150, 30, 220, 20);
         Rectangle GameBorderB = new Rectangle(150, 430, 220, 20);
 
-        
-        //List<Rectangle> Tpieces = new List<Rectangle>();
-      
-        
-      
+
+        List<Rectangle> Tpieces = new List<Rectangle>();
+
         List<List<Rectangle>> pieces = new List<List<Rectangle>>();
-       
+
 
         int playerScore = 0;
 
         string gameState = "waiting";
 
-        bool aDown = false; 
+        bool aDown = false;
         bool dDown = false;
         bool jDown = false;
-       
+
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -46,86 +45,109 @@ namespace tetris
         public Form1()
         {
             InitializeComponent();
-            //pieces.Add(Tpiece1);
-            //pieces.Add(Tpiece2);
-            //pieces.Add(Tpiece3);
-            //pieces.Add(Tpiece4);
-            //pieces.Add(Tpiece5);
 
-           // pieces[0].Add(Tpiece1);
-            //pieces[1].Add(Tpiece4);
-            //pieces[2].Add(Tpiece2);
-            //pieces[3].Add(Tpiece3);
-            //pieces[4].Add(Tpiece1);
-            //Rectangle test = pieces[0][0];
+            Tpieces.Add(Tpiece1);
+            Tpieces.Add(Tpiece2);
+            Tpieces.Add(Tpiece3);
+            Tpieces.Add(Tpiece4);
+            Tpieces.Add(Tpiece5);
+
+            pieces.Add(Tpieces);
+
+
         }
 
+        int counter = 0;
         private void timer_Tick(object sender, EventArgs e)
         {
-            //move piece down
-            Tpiece1.Y += 20;
-            Tpiece4.Y += 20;
-            Tpiece3.Y += 20;
-            Tpiece2.Y += 20;
-            Tpiece5.Y += 20;
-
-            //left right
-            if (aDown == true)
+            for (int j = 0; j < 5; j++)
             {
-                Tpiece1.X -= 20;
-                Tpiece4.X -= 20;
-                Tpiece3.X -= 20;
-                Tpiece2.X -= 20;
-                Tpiece5.X -= 20;
-            }
-            if (dDown == true)
-            {
-                Tpiece1.X += 20;
-                Tpiece4.X += 20;
-                Tpiece3.X += 20;
-                Tpiece2.X += 20;
-                Tpiece5.X += 20;
-            }
-            //rotate
-            if (jDown == true)
-            {
-                Rectangle temp = Tpiece1;
-
-                Tpiece1 = Tpiece2;
-                Tpiece2 = Tpiece3;
-                Tpiece3 = Tpiece5;
-                Tpiece5 = temp;
+                int y = pieces[pieces.Count - 1][j].Y;
+                y += 20;
+                Rectangle tempRec = new Rectangle(pieces[pieces.Count - 1][j].X, y, 20, 20);
+                pieces[pieces.Count - 1][j] = tempRec;
 
             }
 
-
-            //intersection
-            if (Tpiece1.IntersectsWith(GameBorderB) || Tpiece3.IntersectsWith(GameBorderB) || Tpiece2.IntersectsWith(GameBorderB))
+            for (int j = 0; j < 4; j++)
             {
-                Tpiece1.Y -= 20;
-                Tpiece4.Y -= 20;
-                Tpiece3.Y -= 20;
-                Tpiece2.Y -= 20;
-            }
-            if (Tpiece1.IntersectsWith(GameBorderL))
-            {
-                Tpiece1.X += 20;
-                Tpiece4.X += 20;
-                Tpiece3.X += 20;
-                Tpiece2.X += 20;
-            }
-            if (Tpiece3.IntersectsWith(GameBorderR))
-            {
-                Tpiece1.X -= 20;
-                Tpiece4.X -= 20;
-                Tpiece3.X -= 20;
-                Tpiece2.X -= 20;
-            }
+                int y = pieces[pieces.Count - 1][j].Y;
+                y += 20;
+                if (pieces[0][j].IntersectsWith(GameBorderB))
+                {
+                    Rectangle Tpiece1 = new Rectangle(230, 30, 20, 20); //start left
+                    Rectangle Tpiece4 = new Rectangle(250, 30, 20, 20); //start middle
+                    Rectangle Tpiece3 = new Rectangle(270, 30, 20, 20); //start right
+                    Rectangle Tpiece2 = new Rectangle(250, 10, 20, 20); //start top
+                    Rectangle Tpiece5 = new Rectangle(250, 50, 20, 20); //invis, rotate. start bottom
+                    List<Rectangle> list = new List<Rectangle>();
 
+                    list.Add(Tpiece1);
+                    list.Add(Tpiece2);
+                    list.Add(Tpiece3);
+                    list.Add(Tpiece4);
+                    list.Add(Tpiece5);
+                    pieces.Add(list);
 
+                    counter = 0;
 
-            Refresh();
+                }
+                //left right
+
+                if (aDown == true)
+                {
+                    Tpiece1.X -= 20;
+                    Tpiece4.X -= 20;
+                    Tpiece3.X -= 20;
+                    Tpiece2.X -= 20;
+                    Tpiece5.X -= 20;
+                }
+                if (dDown == true)
+                {
+                    Tpiece1.X += 20;
+                    Tpiece4.X += 20;
+                    Tpiece3.X += 20;
+                    Tpiece2.X += 20;
+                    Tpiece5.X += 20;
+                }
+                //rotate
+                if (jDown == true)
+                {
+                    Rectangle temp = Tpiece1;
+
+                    Tpiece1 = Tpiece2;
+                    Tpiece2 = Tpiece3;
+                    Tpiece3 = Tpiece5;
+                    Tpiece5 = temp;
+
+                    //  intersection
+                    if (Tpiece1.IntersectsWith(GameBorderB) || Tpiece3.IntersectsWith(GameBorderB) || Tpiece2.IntersectsWith(GameBorderB))
+                    {
+                        Tpiece1.Y -= 20;
+                        Tpiece4.Y -= 20;
+                        Tpiece3.Y -= 20;
+                        Tpiece2.Y -= 20;
+                    }
+                    if (Tpiece1.IntersectsWith(GameBorderL))
+                    {
+                        Tpiece1.X += 20;
+                        Tpiece4.X += 20;
+                        Tpiece3.X += 20;
+                        Tpiece2.X += 20;
+                    }
+                    if (Tpiece3.IntersectsWith(GameBorderR))
+                    {
+                        Tpiece1.X -= 20;
+                        Tpiece4.X -= 20;
+                        Tpiece3.X -= 20;
+                        Tpiece2.X -= 20;
+                    }
+                }
+                Refresh();
+            }
         }
+
+
 
         private void GameInitalize()
         {
@@ -190,16 +212,23 @@ namespace tetris
             }
             else if (gameState == "running")
             {
-                e.Graphics.FillRectangle(purpleBrush, Tpiece1);
-                e.Graphics.FillRectangle(purpleBrush, Tpiece4);
-                e.Graphics.FillRectangle(purpleBrush, Tpiece3);
-                e.Graphics.FillRectangle(purpleBrush, Tpiece2);
-               
+
+
                 e.Graphics.FillRectangle(whiteBrush, GameBorderL);
                 e.Graphics.FillRectangle(whiteBrush, GameBorderR);
                 e.Graphics.FillRectangle(whiteBrush, GameBorderT);
                 e.Graphics.FillRectangle(whiteBrush, GameBorderB);
+
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        e.Graphics.FillRectangle(purpleBrush, pieces[i][j]);
+                    }
+                }
+
             }
         }
     }
 }
+
